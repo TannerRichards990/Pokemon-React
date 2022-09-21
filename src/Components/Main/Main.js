c4ac870fb2a17b7bf67b220402c6452394111cd8
Lawson <lawsonmcphetridge2020@gmail.com>
2022-09-21 16:58:16 -0700

    put list in a component

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import usePokemon from '../../Hooks/usePokemon';
-import PokemonCard from '../PokemonCard/PokemonCard';
+import PokeList from '../PokeList/PokeList';
+
 
 export default function Main() {
-  const { pokemon } = usePokemon();
+  
   return (
     <div className='main'>
-      {pokemon.map((pokemon) => (
-        <PokemonCard key={pokemon.id} pokemon={pokemon}></PokemonCard>
-      ))}
+      <PokeList />
     </div>
   );
 }
--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './PokemonCard.css';
 
 export default function PokemonCard({ pokemon }) {
   return (
-    <div>
+    <div className='pokemon-container'>
+      <img className='pokemon-img' src={pokemon.url_image} alt={pokemon.pokemon} />
       <h2>Name: {pokemon.pokemon}</h2>
       <h2>Type 1: {pokemon.type_1}</h2>
       <h2>Type 2: {pokemon.type_2}</h2>
       <h2>HP: {pokemon.hp}</h2>
-      <img src={pokemon.url_image} alt={pokemon.pokemon} />
     </div>
   );
 }
