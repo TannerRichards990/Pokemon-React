459fc2c0ad1d68c433fb3718976e14f0fc39bfbc
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-21 10:41:51 -0700

    built out my first fetch function to get the pokemon API

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,3 +1,5 @@
 export async function fetchPokemon() {
-  const reponse = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex')
+  const response = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex');
+  const data = await response.json();
+  return data.results;
 }
