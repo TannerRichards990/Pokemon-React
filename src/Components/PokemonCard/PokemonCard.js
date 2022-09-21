ce649e6ca26a25cf1868b53da3aaeaab549d9dfc
Lawson <lawsonmcphetridge2020@gmail.com>
2022-09-21 16:46:22 -0700

    moved render funcionality into a card

---
+++ src/Components/PokemonCard/PokemonCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function PokemonCard({ pokemon }) {
+  return (
+    <div>
+      <h2>Name: {pokemon.pokemon}</h2>
+      <h2>Type 1: {pokemon.type_1}</h2>
+      <h2>Type 2: {pokemon.type_2}</h2>
+      <h2>HP: {pokemon.hp}</h2>
+      <img src={pokemon.url_image} alt={pokemon.pokemon} />
+    </div>
+  );
+}
