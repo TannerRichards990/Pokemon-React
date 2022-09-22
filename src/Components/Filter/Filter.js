03681c54b9c9411ca09fb788e06f5c95624bb17a
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-22 12:37:22 -0700

    fixed ALL to all in Filter.js so that when the user clicks back to all on the drop down sorter, all pokemon are returned

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -5,7 +5,7 @@ export default function Filter({ types, type, setType }) {
     <div className="filter">
       <select value={type} onChange={(e) => 
         setType(e.target.value)}>
-        <option value="All">All</option>
+        <option value="all">All</option>
         {types.map((type) => (
           <option key={type} value={type}>
             {type}
